88a14ec02f8f66360546bd21ab01bb4a71a1cc93
Mert Topcu <merttopcu.dev@gmail.com>
2024-09-09 22:58:19 +0300

    Add project files.

---
+++ Controllers/CustomerOrdersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatikaBasicMVCTask.Models;
+
+namespace PatikaBasicMVCTask.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            Customer customer = new Customer()
+            {
+                CustomerID = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com"
+            };
+
+            Order order1 = new Order()
+            {
+                Id = 1,
+                Price = 1000,
+                ProductName = "Phone",
+                Quantity = 1,
+            };
+
+            Order order2 = new Order()
+            {
+                Id = 2,
+                Price = 1300,
+                ProductName = "PC",
+                Quantity = 1,
+            };
+
+            CustomerOrderViewModel customerOrderViewModel = new CustomerOrderViewModel()
+            {
+                Customer = customer,
+                Orders = new List<Order> {
+            order1, order2
+            }
+            };
+
+
+
+            return View(customerOrderViewModel);
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace PatikaBasicMVCTask.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
